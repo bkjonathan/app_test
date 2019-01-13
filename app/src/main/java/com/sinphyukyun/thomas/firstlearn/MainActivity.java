5def217e9fb189f392a74ca5e203df66ecf1c284
bkjonathan <admin@sinphyukyun.com>
2019-01-13 23:23:23 +0630

    Init Project

---
+++ app/src/main/java/com/sinphyukyun/thomas/firstlearn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sinphyukyun.thomas.firstlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
